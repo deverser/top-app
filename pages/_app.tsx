314f56bca0bc4d5abe13449e7f33907ce393a006
Alexander <alexmal74@gmail.com>
2022-04-26 01:42:02 +0500

    Head of app redefined

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return <>
+		<Head>
+			<title>MyTop - наш лучший топ</title>
+			<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+		</Head>
+		<Component {...pageProps} />;
+	</>;
+
 }
 
 export default MyApp;
