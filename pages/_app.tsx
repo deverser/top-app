e374140273e7e4535a65b7373215d6053f23115c
Alexander <alexmal74@gmail.com>
2022-04-26 13:51:16 +0500

    linked fonts, created color css-variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,11 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 		<Head>
 			<title>MyTop - наш лучший топ</title>
 			<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
 		</Head>
-		<Component {...pageProps} />;
+		<Component {...pageProps} />
 	</>;
 
 }
