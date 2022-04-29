c19a68dc2ef3cb9a7d71c1dfac6170888238dea7
Alexander <alexmal74@gmail.com>
2022-04-30 02:35:34 +0500

    extended button props

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag='h1'>Текст</Htag>
-			<Button appearance='primary'>Кнопка</Button>
+			<Button appearance='primary' className='dfghfgdfg'>Кнопка</Button>
 			<Button appearance='ghost'>Кнопка2</Button>
 		</>);
 }
