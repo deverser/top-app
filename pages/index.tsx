523d0f2e65df7762d0ff373980992beaf5e1474b
Alexander <alexmal74@gmail.com>
2022-05-01 02:31:47 +0500

    added config for work with SVG-elems

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag='h1'>Текст</Htag>
-			<Button appearance='primary' className='dfghfgdfg'>Кнопка</Button>
-			<Button appearance='ghost'>Кнопка2</Button>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка2</Button>
 		</>);
 }
