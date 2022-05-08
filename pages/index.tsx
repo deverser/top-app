30f3acafe5f615a77fe0e36ad609f00b636283d7
Alexander <alexmal74@gmail.com>
2022-05-09 02:06:46 +0500

    Ended rating element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
-	const [counter, setCounter] = useState<number>(0);
+	const [rating, setRating] = useState<number>(4);
 
-	useEffect(() => {
-		console.log('Counter ' + counter);
-		return function cleanup() {
-			console.log('Unmount');
-		};
-	});
-
-	useEffect(() => {
-		console.log('Mounted');
-	}, []);
 	return (
 		<>
-			<Htag tag='h1'>{counter}</Htag>
-			<Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
+			<Htag tag='h1'>Заголовок</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка2</Button>
 			<P size='l'>Большой</P>
 			<P>Средний</P>
@@ -26,7 +16,7 @@ export default function Home(): JSX.Element {
 			<Tag size='m' color='red'>Red</Tag>
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Primary</Tag>
-			<Rating rating={4} />
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	);
 }
