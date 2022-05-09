f7ada9a96a959cdb6da6e831b502ea99df8882db
Alexander <alexmal74@gmail.com>
2022-05-10 02:21:19 +0500

    Added Layout function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
-import { Layout } from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
-		<Layout>
+		<>
 			<Htag tag='h1'>Заголовок</Htag>
 			<Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка2</Button>
@@ -18,6 +18,8 @@ export default function Home(): JSX.Element {
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Primary</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-		</Layout>
+		</>
 	);
 }
+
+export default withLayout(Home);
