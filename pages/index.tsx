132bc19f6e2e46a20b6fe15950bbb4c9b4ea1283
Alexander <alexmal74@gmail.com>
2022-05-19 16:40:12 +0500

    getStaticProps from API Request

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import React, { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
 import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -20,6 +21,9 @@ function Home(): JSX.Element {
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Primary</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
+			<ul>
+				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+			</ul>
 		</>
 	);
 }
@@ -28,7 +32,9 @@ export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps = async () => {
 	const firstCategory = 0;
-	const { data: menu } = await axios.post(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find');
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
 	return {
 		props: {
 			menu,
@@ -36,3 +42,8 @@ export const getStaticProps: GetStaticProps = async () => {
 		}
 	};
 };
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
