ca623a11a886fd7b2bed53fe4a589987103c2108
Alexander <alexmal74@gmail.com>
2022-05-23 16:47:09 +0500

    Started page interface for SSR

---
+++ pages/courses/[alias].tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next';
+import React, { useState } from 'react';
+import { withLayout } from '../../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../../interfaces/menu.interface';
+
+function Course({ menu }: CourseProps): JSX.Element {
+	return (
+		<>
+
+		</>
+	);
+}
+
+export default withLayout(Course);
+
+export const getStaticProps: GetStaticProps<CourseProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<CourseProps[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	const { data: page } = await axios.post<CourseProps[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface CourseProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
