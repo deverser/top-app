c9acbdd6f7cf2fe17413b8c0e1c2915ebee0b421
Alexander <alexmal74@gmail.com>
2022-06-20 23:41:24 +0500

    HhData module layout

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,8 +1,9 @@
 import { Card } from '../Card/Card';
 import styles from './HhData.module.css';
 import { HhDataProps } from './HhData.props';
+import RateIcon from './rate.svg';
 
-export const HhData = ({ count }: HhDataProps): JSX.Element => {
+export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDataProps): JSX.Element => {
 	return (
 		<div className={styles.hh}>
 			<Card className={styles.count}>
@@ -10,9 +11,33 @@ export const HhData = ({ count }: HhDataProps): JSX.Element => {
 				<div className={styles.countValue}>{count}</div>
 			</Card>
 			<Card className={styles.salary}>
-				<div className={styles.title}>Всего вакансий</div>
-				<div className={styles.salaryValue}>{count}</div>
-				<div className={styles.rate}></div>
+				<div>
+					<div className={styles.title}>Начальный</div>
+					<div className={styles.salaryValue}>{juniorSalary}</div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled} />
+						<RateIcon />
+						<RateIcon />
+					</div>
+				</div>
+				<div>
+					<div className={styles.title}>Средний</div>
+					<div className={styles.salaryValue}>{middleSalary}</div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled} />
+						<RateIcon className={styles.filled} />
+						<RateIcon />
+					</div>
+				</div>
+				<div>
+					<div className={styles.title}>Профессионал</div>
+					<div className={styles.salaryValue}>{seniorSalary}</div>
+					<div className={styles.rate}>
+						<RateIcon className={styles.filled} />
+						<RateIcon className={styles.filled} />
+						<RateIcon className={styles.filled} />
+					</div>
+				</div>
 			</Card>
 		</div>
 	);
