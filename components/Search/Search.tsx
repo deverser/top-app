a80db96281f0be67d00ea800f59271611a1e0ed8
Alexander <alexmal74@gmail.com>
2022-08-02 16:42:20 +0500

    Started work on search component

---
+++ components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import { SearchProps } from './Search.props';
+import styles from './Search.module.css';
+import cn from 'classnames';
+
+export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
+	return (
+		<div>
+
+		</div>
+	);
+};
