ddeb4eb84bc6e0f093a5d86d41d9cc72a3397780
Alexander <alexmal74@gmail.com>
2022-08-05 15:35:24 +0500

    Started work on Product Card component

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,11 +1,19 @@
 import { ProductProps } from './Product.props';
 import cn from 'classnames';
 import styles from './Product.module.css';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
-		<div>
-			{product.title}
-		</div>
+		<Card>
+			<div className={styles.logo}><img src={product.image} alt={product.title} /></div>
+			<div className={styles.title}>{product.title}</div>
+			<div className={styles.price}>{product.price}</div>
+			<div className={styles.credit}>{product.credit}</div>
+			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
+			<div className={styles.tag}>{product.categories.map((c) => <Tag color='ghost' key={c}>{c}</Tag>)}</div>
+		</Card>
 	);
 }; 
