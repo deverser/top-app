a72f842a264148fced04823f971e29fe1e87ed76
Alexander <alexmal74@gmail.com>
2022-12-27 16:44:21 +0500

    Handling review form errors

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -3,6 +3,12 @@ import cn from 'classnames';
 import styles from './Input.module.css';
 import { ForwardedRef, forwardRef } from 'react';
 
-export const Input = forwardRef(({ className, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
-	return (<input className={cn(styles.input, className)} ref={ref} {...props} />);
+export const Input = forwardRef(({ className, error, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
+	return (
+		<div className={cn(styles.inputWrapper, className)}>
+			<input className={cn(styles.input, { [styles.error]: error })} ref={ref} {...props} />
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
+		</div>
+
+	);
 }); 
