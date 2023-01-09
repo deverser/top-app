42bdc2aed8fc0d43443d794bcd79f438f71a4ec6
Alexander M <alexmal74@gmail.com>
2023-01-09 15:05:31 +0500

    Handling rating error in feedback

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -4,7 +4,7 @@ import StarIcon from './star.svg';
 import cn from 'classnames';
 import { useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef } from 'react';
 
-export const Rating = forwardRef(({ isEditable = false, rating, setRating, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const Rating = forwardRef(({ isEditable = false, rating, setRating, error, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
 	useEffect(() => {
@@ -55,8 +55,9 @@ export const Rating = forwardRef(({ isEditable = false, rating, setRating, ...pr
 	};
 
 	return (
-		<div {...props} ref={ref}>
+		<div {...props} ref={ref} className={cn(styles.ratingWrapper, { [styles.error]: error })}>
 			{ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
 		</div>
 	);
 });
